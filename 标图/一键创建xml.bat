2d8618a043ac6cc2f740dc7012e72061ba74008c
likeTianDian <yang2218611984>
2020-10-13 17:28:42 +0800

    无

---
+++ 标图/一键创建xml.bat
@@ -0,0 +1,24 @@
+@echo off
+echo ==============================
+echo 不知道原理的别乱用，坏了别怪人
+echo.
+echo                    by TianDian
+echo ==============================
+pause
+dir /ad /b > dir.txt
+echo 本文档显示未建立xml的文件夹>log.txt
+for /f %%i in (dir.txt) do (
+    cd %%i
+    if exist *.xml (
+        echo 文件夹 %%i 存在xml，已跳过
+    ) else (
+        dir /ad /b > dir.txt
+        for /f %%j in (dir.txt) do (
+            imglab.exe -c %%j.xml %%j
+        )
+    )
+    del dir.*
+    cd ..
+)>>log.txt
+del dir.txt
+echo xml已创建完成，按任意键退出
