3ae3d3b65d6dbb513967ef9aba7fa031cc10a4c3
likeTianDian <68725135+likeTianDian@users.noreply.github.com>
2021-03-24 17:26:03 +0800

    Update 建立明天文件夹.bat
    以前是单独对最后日期进行加1后再添加分隔符,现在更改为整体加1

--- 建立明天文件夹.bat
+++ 建立明天文件夹.bat
@@ -1,2 +1,4 @@
-set /a timea=%date:~8,2%+1
-mkdir %date:~0,4%%date:~5,2%%timea%
+@echo off
+set /a timea=%date:~0,4%%date:~5,2%%date:~8,2%
+set /a timeb=%timea%+1
+mkdir %timeb:~0,4%-%timeb:~4,2%-%timeb:~6,2%
